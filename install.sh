5e5e99c696258c0ae67d34407e6a8644340dbc75
kodeaqua <anajmizdn@gmail.com>
2021-05-28 01:49:36 +0700

    Fix wget error

--- install.sh
+++ install.sh
@@ -5,17 +5,15 @@ DEST=/usr/bin/xampp
 if ! [[ -d "$XAMPP" ]]; then
 
 	# Upstream 
-	MAJOR="8.0.6" && PATCH="0" && XAMPPVERSION='"$MAJOR"-"$PATCH"'
-	URL='https://www.apachefriends.org/xampp-files/"$MAJOR"/xampp-linux-x64-"$XAMPPVERSION"-installer.run'
+	URL='https://www.apachefriends.org/xampp-files/8.0.6/xampp-linux-x64-8.0.6-0-installer.run'
 	
 	if ! [[ $(which wget) ]]; then
 		sudo apt update && sudo apt install -y wget
 	fi
 
-	if ! [[ -f 'xampp-linux-x64-"$XAMPPVERSION"-installer.run' ]]; then
-		wget -O 'xampp-linux-x64-"$XAMPPVERSION"-installer.run' "$URL"	
+	if ! [[ -f 'xampp-linux-*-installer.run' ]]; then
+		wget --no-clobber "$URL"
 	fi
-
 	chmod 755 xampp-linux-*-installer.run && sudo ./xampp-linux-*-installer.run
 
 fi
