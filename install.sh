68b3931396f05925ddb715261e26bd838a966480
kodeaqua <anajmizdn@gmail.com>
2021-05-28 01:31:56 +0700

    Add XAMPP installer

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+XAMPP=/opt/lampp
 DEST=/usr/bin/xampp
+
+if ! [[ -d "$XAMPP" ]]; then
+
+	# Upstream 
+	MAJOR="8.0.6" && PATCH="0" && XAMPPVERSION='"$MAJOR"-"$PATCH"'
+	URL='https://www.apachefriends.org/xampp-files/"$MAJOR"/xampp-linux-x64-"$XAMPPVERSION"-installer.run'
+	
+	if ! [[ $(which wget) ]]; then
+		sudo apt update && sudo apt install -y wget
+	fi
+
+	if ! [[ -f 'xampp-linux-x64-"$XAMPPVERSION"-installer.run' ]]; then
+		wget -O 'xampp-linux-x64-"$XAMPPVERSION"-installer.run' "$URL"	
+	fi
+
+	chmod 755 xampp-linux-*-installer.run && sudo ./xampp-linux-*-installer.run
+
+fi
+
 sudo cp "xampp" "$DEST" && sudo chmod 0755 "$DEST"
