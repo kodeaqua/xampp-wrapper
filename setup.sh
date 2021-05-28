7193983aeae43ca5c8af060922e444da123ce6e4
kodeaqua <anajmizdn@gmail.com>
2021-05-28 11:05:02 +0700

    Update 'setup.sh'

--- setup.sh
+++ setup.sh
@@ -2,24 +2,37 @@
 XAMPP=/opt/lampp
 DEST=/usr/bin/xampp
 
-if ! [[ -d "$XAMPP" ]]; then
+if [[ $1 == 'install' ]]; then
 
-	# Upstream 
-	URL='https://www.apachefriends.org/xampp-files/8.0.6/xampp-linux-x64-8.0.6-0-installer.run'
+	if ! [[ -d "$XAMPP" ]]; then
+
+		# Upstream 
+		URL='https://www.apachefriends.org/xampp-files/8.0.6/xampp-linux-x64-8.0.6-0-installer.run'
 	
-	if ! [[ $(which wget) ]]; then
-		sudo apt update && sudo apt install -y wget
+		if ! [[ $(which wget) ]]; then
+			sudo apt update && sudo apt install -y wget
+		fi
+
+		if ! [[ $(which netstat) ]]; then
+			sudo apt update && sudo apt install -y net-tools	
+		fi
+
+		if ! [[ -f 'xampp-linux-*-installer.run' ]]; then
+			wget --no-clobber "$URL"
+		fi
+
+		chmod 755 xampp-linux-*-installer.run && sudo ./xampp-linux-*-installer.run
+
+		sudo cp "xampp" "$DEST" && sudo chmod 0755 "$DEST"
+
 	fi
 
-	if ! [[ $(which netstat) ]]; then
-		sudo apt update && sudo apt install -y net-tools	
-	fi
+elif [[ $1 == 'uninstall' ]]; then
 
-	if ! [[ -f 'xampp-linux-*-installer.run' ]]; then
-		wget --no-clobber "$URL"
-	fi
-	chmod 755 xampp-linux-*-installer.run && sudo ./xampp-linux-*-installer.run
+	sudo $XAMPP/uninstall && sudo rm -rf "$XAMPP"
+
+else
+
+	printf "Too few or invalid argument!"
 
 fi
-
-sudo cp "xampp" "$DEST" && sudo chmod 0755 "$DEST"
